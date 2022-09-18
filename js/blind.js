7a5a6a4491042a609aaecece0fdafb5c7ab7c25c
Roman <generallevy45@mail.ru>
2022-09-18 23:51:51 +0300

    update blind

--- js/blind.js
+++ js/blind.js
@@ -1,48 +1,35 @@
-let btn = document.getElementById("btn");
-let play = document.getElementById("play");
-let inputLetters = document.querySelector(".letters");
-
-
-play.addEventListener("click", function() {
-    window.location.reload();
-});
-
-function getRandomNumber(max) {
-    return Math.round(Math.random() * max);
-}
-
-function generateText(letter, length) {
-    let text = "";
-    for(let i = 0; i < length; i++) {
-        let n = getRandomNumber(letter.length - 1);
-        text = text + letter[n];
+const app = new Vue({
+    el: "#app",
+    data: {
+      letters: "",
+      text: "",
+      answer: "",
+      count: 0
+    },
+    methods: {
+      getRandomNumber(to) {
+        return Math.round(Math.random() * to);
+      },
+      generateText(letters, length) {
+        let text = "";
+        for (let i = 0; i < length; i++) {
+          let n = this.getRandomNumber(letters.length - 1);
+          text += letters[n];
+        }
+        return text;
+      },
+      getNewText() {
+        let arrayLetters = this.letters.split(",");
+        this.text = this.generateText(arrayLetters, 20);
+      },
+      checkedAnswer() {
+        if (this.answer === this.text) {
+          this.count++;
+          this.answer = "";
+          this.getNewText();
+        } else {
+          this.answer = "";
+        }
+      }
     }
-    return text;
-}
-
-function addText(id, text) {
-    document.getElementById(id).innerHTML = text;
-}
-
-
-document.querySelector(".add__text-btn").addEventListener("click", () => {
-    let letters = inputLetters.value.split(",");
-    let text = generateText(letters, 20);   
-    addText("text", text);
-    addText("info", "");
-});
-
-function userAnswer(id) {
-    return document.querySelector(id).value;
-}
-
-btn.addEventListener("click", function() {
-    let answer = userAnswer("#userAnswer");
-    let text = document.querySelector("#text").textContent;
-    if(answer == text) {
-        addText("info", "Поздравляю, у вас получилось.");
-    } else {
-        addText("info", "Вы допустили ошибку, попробуйте еще раз.");
-    }
-});
-
+  });
